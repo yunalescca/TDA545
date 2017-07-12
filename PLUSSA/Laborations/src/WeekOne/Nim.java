e3fb6369c4986e49fda30c715200920e70c484b2
Josefin <yunalescca@gmail.com>
2017-07-12 19:01:34 +0200

    Week one's laboration covering the NIM game

---
+++ PLUSSA/Laborations/src/WeekOne/Nim.java
@@ -0,0 +1,15 @@
+package WeekOne;
+
+/**
+ * Created by Yunalescca on 2017-07-12.
+ */
+public class Nim {
+
+    public Nim(){
+ 
+    }
+
+    public static void main(String[] args){
+        new Nim();
+    }
+}
